76471f82e57c7ca9c943ad6d99eb36458fe0b09a
Manish Sahu <manish_m160081ca@nitc.ac.in>
2018-08-26 11:27:49 +0530

    showing album names

--- music/views.py
+++ music/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album,Song
+from django.template import loader
 # Create your views here.
+
 def index(request):
-    return HttpResponse('thsi is music app index page')
+    all_albums=Album.objects.all()
+    html=''
+    for a in all_albums:
+        url='/music/'+str(a.id)+'/'
+        html+='<a href="'+url + '">'+a.album_title+'</a><br>'
+        return HttpResponse(html)
+
+def detail(request,album_id):
+    # print(HttpResponse('Album.get(album_id)')
+    #  all_albums=Album.objects.all()
+    #  html=''
+    #  for a in all_albums:
+    #      if a.id==albumid:
+    #          html+=
+
+     return HttpResponse("<h1> this is album id :"+ str(album_id) +"</h2>")
+    
