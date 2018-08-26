33fc7c2de08c5a9113d0db8e238cfc3dc456e0bf
Manish Sahu <manish_m160081ca@nitc.ac.in>
2018-08-26 17:15:43 +0530

    making a formm to add favorite

--- music/views.py
+++ music/views.py
@@ -1,24 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from .models import Album,Song
-from django.template import loader
+from django.http import Http404
 # Create your views here.
 
 def index(request):
     all_albums=Album.objects.all()
-    html=''
-    for a in all_albums:
-        url='/music/'+str(a.id)+'/'
-        html+='<a href="'+url + '">'+a.album_title+'</a><br>'
-        return HttpResponse(html)
+    # context={
+    #     'all_albums': all_albums
+    # }
+    return render(request,'music/index.html',{'all_albums':all_albums})
+    # for a in all_albums:
+    #     url='/music/'+str(a.id)+'/'
+    #     html+='<a href="'+url + '">'+a.album_title+'</a><br>'
+       
 
 def detail(request,album_id):
+    all_albums=Album.objects.all()
+    context={
+        'all_albums': all_albums
+    }
     # print(HttpResponse('Album.get(album_id)')
     #  all_albums=Album.objects.all()
     #  html=''
     #  for a in all_albums:
     #      if a.id==albumid:
     #          html+=
+    # try:
+    #     album=Album.objects.get(pk=album_id)
+    # except Album.DoesNotExist:
+    #     raise Http404("Album does not exist")
+    album=get_object_or_404(Album,pk=album_id) # replacement of try and except 
+    return render(request,'music/detail.html',{'album':album})
 
-     return HttpResponse("<h1> this is album id :"+ str(album_id) +"</h2>")
-    
+
+def favorite(request,album_id):
+    album=get_object_or_404(Album,pk=album_id)
+    try:
+        selected_song=album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request,'music/detail.html',{'album':album,'error_message':"you did not select any song"})
+    else:
+        selected_song.is_favorite=True
+        selected_song.save()
+        return render(request,'music/detail.html',{'album':album})
+
+
